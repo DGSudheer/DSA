bce560b49beb7fb0cf78f34193cece37b8dd7ab7
D G Sudheer <dgsudheer@gmail.com>
2025-06-04 21:15:07 +0530

    pattern12

---
+++ pattern12.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void pattern12(int n) {
+        int blank=2*(n-1);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int k=1;k<=blank;k++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+            blank -=2;
+        }
+    }
+}
