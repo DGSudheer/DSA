5df1bbdda850f6eaf572eb6e53b803f8d13be4fc
D G Sudheer <dgsudheer@gmail.com>
2025-06-09 08:15:22 +0530

    Reverse array

---
+++ reverseArray.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] reverseArray(int[] nums) {
+        // Call the helper function to reverse the array
+        reverse(nums, 0, nums.length - 1); 
+        // Return the reversed array
+        return nums; 
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        // Base case: pointers have crossed, the array is reversed
+        if (left >= right) {
+            return; 
+        }
+        // Swap the elements
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        // Recursive call with updated pointers
+        reverse(nums, left + 1, right - 1); 
+    }
+}
+
+// Main method for testing the reverseArray function
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 2, 3, 4, 5};        
+        int[] result = solution.reverseArray(nums); 
+        for (int num : result) {
+            System.out.print(num + " "); 
+        }
+    }
+}
+
+class Solution {
+    public int[] reverseArray(int[] nums) {
+        //your code goes here
+        int[] revnums=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            revnums[nums.length-i-1]=nums[i];
+        }
+        return revnums;
+    }
+}
